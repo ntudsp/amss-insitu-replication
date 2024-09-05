e31dbac790cfe50f139de8d0eae1f70ae19cfe38
bhanlam <blam002@e.ntu.edu.sg>
2024-09-05 16:32:59 +0800

    updated stat results file

--- scripts/supportScripts.R
+++ scripts/supportScripts.R
@@ -7,7 +7,9 @@ twoWLMERMANOVA <- function(metadata, dataframe) {
                 term=character(),
                 test=character(),
                 p.value=character(),
-                eff.size=character()
+                eff.size=character(),
+                estimate=numeric(),
+                contrast=character()
         )        
         
         #Determine if non-parametric
@@ -19,7 +21,7 @@ twoWLMERMANOVA <- function(metadata, dataframe) {
                            "\n"))
                 variable_list <- c(
                         "variable","term","test",
-                        "p.value","eff.size"
+                        "p.value","eff.size","estimate","contrast"
                 )
                 #filter out site=="MP" if not PA/NA
                 if(!variable %in% c("PosAff","NegAff")){
@@ -61,7 +63,9 @@ twoWLMERMANOVA <- function(metadata, dataframe) {
                                         test=method,
                                         term="Residuals",
                                         eff.size=NA,
-                                        variable=variable
+                                        variable=variable,
+                                        estimate=NA,
+                                        contrast=NA
                                         ) |>
                                 dplyr::select(all_of(variable_list))
                         
@@ -97,27 +101,80 @@ twoWLMERMANOVA <- function(metadata, dataframe) {
                         lme.model.eff<-omega_squared(lme.model)
                 }
                 
+                # After fitting the lme.model, calculate contrasts
+                contrasts <- emmeans(lme.model, specs = c("site", "condition"))
+                site_contrasts <- contrast(contrasts, method = "pairwise", by = "condition")
+                condition_contrasts <- contrast(contrasts, method = "pairwise", by = "site")
+                
+                
+                #extract fixed effect estimates
+                print(lme4::fixef(lme.model))
+                
                 #compute lme model using Type 3 ANOVA
                 lme.model.anova3<-car::Anova(lme.model,type=3)
-                lme.model.main<-broom::tidy(lme.model.anova3)
+                #lme.model.main<-broom::tidy(lme.model.anova3)
+                
+                lme.model.main <- broom::tidy(lme.model.anova3) |>
+                        dplyr::filter(!term == "(Intercept)") |>
+                        dplyr::select(term, statistic, p.value) |>
+                        cbind(lme.model.eff$Omega2_partial) |>
+                        dplyr::mutate(
+                                test = ifelse(category == "categorical" || 
+                                                      int.norm$p.value < 0.05,
+                                              "2ME-RT-RMANOVA",
+                                              "2ME-RMANOVA"),
+                                eff.size = `lme.model.eff$Omega2_partial`,
+                                variable = variable,
+                                estimate = NA,
+                                contrast = NA
+                        ) |>
+                        dplyr::select(all_of(variable_list))
                 
                 #p.val of interaction effect
                 interaction.p.val<-lme.model.main[
                         lme.model.main$term=="site:condition",]$p.value
                 
                 #combine effect size and p val
-                lme.model.main <- lme.model.main |> 
-                        dplyr::filter(!term=="(Intercept)") |>
-                        dplyr::select(term,p.value) |>
-                        cbind(lme.model.eff$Omega2_partial) |>
-                        dplyr::mutate(
-                                test=ifelse(category=="categorical" || 
-                                                    int.norm$p.value<0.05,
-                                            "2ME-RT-RMANOVA",
-                                            "2ME-RMANOVA"),
-                                eff.size=`lme.model.eff$Omega2_partial`,
-                                variable=variable) |>
-                        dplyr::select(all_of(variable_list))
+                # lme.model.main <- lme.model.main |> 
+                #         dplyr::filter(!term=="(Intercept)") |>
+                #         dplyr::select(term,p.value) |>
+                #         cbind(lme.model.eff$Omega2_partial) |>
+                #         dplyr::mutate(
+                #                 test=ifelse(category=="categorical" || 
+                #                                     int.norm$p.value<0.05,
+                #                             "2ME-RT-RMANOVA",
+                #                             "2ME-RMANOVA"),
+                #                 eff.size=`lme.model.eff$Omega2_partial`,
+                #                 variable=variable) |>
+                #         dplyr::select(all_of(variable_list))
+                
+                # # Add contrast estimates to lme.model.main
+                # site_estimates <- summary(site_contrasts) %>%
+                #         group_by(condition) %>%
+                #         summarise(estimate = estimate[1], contrast = contrast[1], .groups = "drop") %>%
+                #         mutate(term = "site") #|>
+                #         #dplyr::select(!condition)
+                # 
+                # condition_estimates <- summary(condition_contrasts) %>%
+                #         group_by(site) %>%
+                #         summarise(estimate = estimate[1], contrast = contrast[1], .groups = "drop") %>%
+                #         mutate(term = "condition") #|>
+                #         #dplyr::select(!site)
+                # 
+                # print(site_estimates)
+                # print(condition_estimates)
+                # 
+                # 
+                # estimates <- rbind(site_estimates, condition_estimates) 
+                # 
+                # lme.model.main <- lme.model.main %>%
+                #         left_join(estimates, by = "term")
+                # 
+                # print(lme.model.main)
+                # 
+                # # Select final columns
+                # lme.model.main <- lme.model.main %>%
+                #         dplyr::select(variable, term, test, p.value, eff.size, estimate, contrast)
                 
                 stat.results <- rbind(stat.results,lme.model.main)
                 
@@ -148,50 +205,72 @@ twoWLMERMANOVA <- function(metadata, dataframe) {
                         site.pairs<-broom::tidy(
                                 pair_comp$`simple contrasts for site`)
                         
-                        #effect size computation
-                        totSD<-sqrt(
-                                sum(as.data.frame(
-                                        nlme::VarCorr(lme.model))$vcov)
-                        )
-                        
-                        cond.eff<-as.data.frame(as.emm_list(eff_size(
-                                pair_comp$`simple contrasts for condition`,
-                                sigma=totSD,
-                                edf=Inf,
-                                method = "identity")))
-                        
-                        site.eff<-as.data.frame(as.emm_list(eff_size(
-                                pair_comp$`simple contrasts for site`,
-                                sigma=totSD,
-                                edf=Inf,
-                                method = "identity")))
-                        
-                        cond.pairs <- cond.pairs |>
-                                cbind(cond.eff$effect.size) |>
-                                dplyr::mutate(
+                        # Add contrast estimates to condition pairs and site pairs
+                        cond.pairs <- summary(condition_contrasts) |>
+                                mutate(
+                                        variable = variable,
                                         term = paste0(contrast, " | ", site),
                                         test = "Simple Contrasts for Condition",
-                                        eff.size = `cond.eff$effect.size`,
-                                        variable = variable
-                                        ) |>       
-                                dplyr::select(all_of(variable_list))
+                                        eff.size = (estimate / sqrt(sum(as.data.frame(VarCorr(lme.model))$vcov)))
+                                ) %>%
+                                dplyr::select(variable, term, test, p.value, eff.size, estimate, contrast)
                         
-                        site.pairs <- site.pairs |>
-                                cbind(site.eff$effect.size) |>
-                                dplyr::mutate(
-                                        term = paste0(contrast," | ",condition),
+                        site.pairs <- summary(site_contrasts) |>
+                                mutate(
+                                        variable = variable,
+                                        term = paste0(contrast, " | ", condition),
                                         test = "Simple Contrasts for Site",
-                                        eff.size = `site.eff$effect.size`,
-                                        variable = variable)
+                                        eff.size = (estimate / sqrt(sum(as.data.frame(VarCorr(lme.model))$vcov)))
+                                ) %>%
+                                dplyr::select(variable, term, test, p.value, eff.size, estimate, contrast)
                         
-                        if (variable %in% c("PosAff","NegAff")) {
-                                site.pairs<-site.pairs |>
-                                        dplyr::mutate(p.value=adj.p.value) |>
-                                        dplyr::select(all_of(variable_list))
-                        } else {
-                                site.pairs <- site.pairs |> 
-                                        dplyr::select(all_of(variable_list))
-                        }        
+                        # #effect size computation
+                        # totSD<-sqrt(
+                        #         sum(as.data.frame(
+                        #                 nlme::VarCorr(lme.model))$vcov)
+                        # )
+                        # 
+                        # cond.eff<-as.data.frame(as.emm_list(eff_size(
+                        #         pair_comp$`simple contrasts for condition`,
+                        #         sigma=totSD,
+                        #         edf=Inf,
+                        #         method = "identity")))
+                        # 
+                        # site.eff<-as.data.frame(as.emm_list(eff_size(
+                        #         pair_comp$`simple contrasts for site`,
+                        #         sigma=totSD,
+                        #         edf=Inf,
+                        #         method = "identity")))
+                        # 
+                        # cond.pairs <- cond.pairs |>
+                        #         cbind(cond.eff$effect.size) |>
+                        #         dplyr::mutate(
+                        #                 term = paste0(contrast, " | ", site),
+                        #                 test = "Simple Contrasts for Condition",
+                        #                 eff.size = `cond.eff$effect.size`,
+                        #                 variable = variable
+                        #                 ) |>       
+                        #         dplyr::select(all_of(variable_list))
+                        # 
+                        # site.pairs <- site.pairs |>
+                        #         cbind(site.eff$effect.size) |>
+                        #         dplyr::mutate(
+                        #                 term = paste0(contrast," | ",condition),
+                        #                 test = "Simple Contrasts for Site",
+                        #                 eff.size = `site.eff$effect.size`,
+                        #                 variable = variable)
+                        
+                        #str(site.pairs)
+                        
+                        # if (variable %in% c("PosAff","NegAff")) {
+                        #         site.pairs<-site.pairs |>
+                        #                 dplyr::mutate(p.value=adj.p.value) |>
+                        #                 dplyr::select(all_of(variable_list))
+                        # } else {
+                        #         site.pairs <- site.pairs |> 
+                        #                 dplyr::select(all_of(variable_list))
+                        # }        
+                        
                         
                         #print(rbind(cond.pairs,site.pairs))
                         
